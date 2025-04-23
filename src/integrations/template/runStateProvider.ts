7fdb17ab421c010b5389c626a8c033edfda8f079
Bruno Wu <wubruno@outlook.com>
2025-04-23 10:47:37 +0200

    Setup unit testing and complete the readme

--- src/integrations/template/runStateProvider.ts
+++
@@ -1,6 +0,0 @@
-import { berachainClient } from "../../config";
-import { TemplatePoolStateProvider } from "./TemplatePoolStateProvider";
-
-const stateProvider = new TemplatePoolStateProvider(berachainClient);
-
-stateProvider.start();
